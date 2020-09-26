b589cb0d8e25696e0c1eb55064d9c328b3839ef5
Chinmaya Pati <chinmaya.pati@loginradius.com>
2020-09-26 14:45:53 +0530

    Added load from env file

--- config/config.go
+++ config/config.go
@@ -1,8 +1,10 @@
 package config
 
 import (
+	"io/ioutil"
 	"log"
 	"os"
+	"strings"
 	"sync"
 
 	"github.com/caarlos0/env"
@@ -27,14 +29,31 @@ var once sync.Once
 func init() {
 	once.Do(func() {
 		instance = Config{}
+
+		loadFromEnvFile(".env")
 		if err := env.Parse(&instance); err != nil {
 			log.Fatal(err)
 		} else {
-
 			os.Setenv("AWS_ACCESS_KEY_ID", instance.AWSKey)
 			os.Setenv("AWS_SECRET_ACCESS_KEY", instance.AWSSecret)
 		}
 	})
+}
+
+func loadFromEnvFile(file string) {
+	envFile, err := ioutil.ReadFile(file)
+	if err == nil {
+		formatted := strings.Split(string(envFile), "\n")
+		for _, val := range formatted {
+			val = strings.Trim(val, "\n")
+			if val != "" && !strings.HasPrefix(val, "#") {
+				envValue := strings.Split(val, "=")
+				if len(envValue) == 2 {
+					os.Setenv(envValue[0], envValue[1])
+				}
+			}
+		}
+	}
 }
 
 // Env returns a config instance
